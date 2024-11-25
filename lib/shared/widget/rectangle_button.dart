0f67675f6bcf4aabeec52afaf3f0d436186c4d75
soonkit-king <kitking2002@gmail.com>
2024-11-26 00:17:29 +0800

    Updated rectangle_button based on app_constants's ColorConstant class

--- lib/shared/widget/rectangle_button.dart
+++ lib/shared/widget/rectangle_button.dart
@@ -1,37 +1,39 @@
 import 'package:flutter/material.dart';
-import '../utils/custom_color.dart';
+import '../constants/app_constants.dart';
 
 class RectangleButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
+  final bool enable;
 
   const RectangleButton({
     super.key,
     required this.label,
     required this.onPressed,
+    this.enable = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        height: 55.0,
-        child: TextButton(
-          onPressed: onPressed,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: CustomColor.darkBlue,
-            foregroundColor: CustomColor.white,
-            disabledBackgroundColor: CustomColor.lightGrey,
-            disabledForegroundColor: CustomColor.grey,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-            padding: const EdgeInsets.symmetric(vertical: 15), // Adjust padding here
-            minimumSize: const Size(double.infinity, 55), // Ensures full-width button with fixed height
-          ),
-          child: Text(
-            label,
-            style: const TextStyle(fontSize: 16),
-          ),
-    )
+    return SizedBox(
+      width: double.infinity,
+      height: 55.0,
+      child: TextButton(
+        onPressed: enable ? onPressed : null,  // Disable onPressed if enable is false
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ColorConstant.darkBlue, // Normal background color
+          foregroundColor: ColorConstant.white, // Normal text color
+          disabledBackgroundColor: ColorConstant.lightGrey, // Background color when disabled
+          disabledForegroundColor: ColorConstant.grey, // Text color when disabled
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          minimumSize: const Size(double.infinity, 55),
+        ),
+        child: Text(
+          label,
+          style: const TextStyle(fontSize: 16),
+        ),
+      ),
     );
   }
 }
