ba70100ea266ed7f78a81f111c61c51eaf298031
soonkit-king <kitking2002@gmail.com>
2024-12-01 01:11:54 +0800

    Restructured menu files, and COMPLETED ride time top-up module

--- lib/modules/menu/widget/modal.dart
+++ lib/modules/menu/widget/modal.dart
@@ -1,44 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:ebikesms/modules/auth/screen/login.dart';
 
+import '../../../shared/constants/app_constants.dart';
+
 Future<void> logoutModal(BuildContext context) async {
   return showDialog<void>(
     context: context,
-    barrierDismissible: true, // user must tap button!
+    barrierDismissible: true, // User must tap button!
     builder: (BuildContext context) {
       return AlertDialog(
+        alignment: Alignment.center,
         content: const SingleChildScrollView(
           child: ListBody(
             children: <Widget>[
-              Center(
-                child: Text('Confirm Logout?'),
+              Padding(
+                padding: EdgeInsets.only(top: 15),
+                child:  Center(child: Text('Confirm Logout?', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),))
               )
             ],
           ),
         ),
         actions: <Widget>[
-          ListTile(
-            title: const Center(
-              child: Text(
-                'Log Out',
-                style: TextStyle(color: Colors.red),
-              ),
+          Container(
+            decoration: const BoxDecoration(
+                border: Border(top: BorderSide(color: ColorConstant.lightGrey))),
+            child: ListTile(
+              title: const Center(child: Text("Logout", style: TextStyle(color: ColorConstant.red),)),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+              },
             ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const LoginScreen()),
-              );
-            },
           ),
-          ListTile(
-            title: const Center(child: Text('Cancel')),
-            onTap: () {
-              Navigator.of(context).pop();
-            },
+          Container(
+            decoration: const BoxDecoration(border: Border(
+                    top: BorderSide(color: ColorConstant.lightGrey))
+            ),
+            child: ListTile(
+              title: const Center(child: Text("Cancel")),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
           ),
         ],
       );
     },
   );
 }
+
+//
+//
+// appBar: AppBar(
+//   title: const Text(
+//   'Learn',
+//   style: TextStyle(color: ColorConstant.white),
+//   ),
+//   backgroundColor: ColorConstant.darkBlue,
+//   leading: IconButton(
+//     onPressed: () {
+//       Navigator.push(context, MaterialPageRoute(builder: (context) => const MenuApp()));
+//     },
+//   icon: SvgPicture.asset('assets/icons/back.svg', width: 24, height: 24)),
+// ),
+
+
