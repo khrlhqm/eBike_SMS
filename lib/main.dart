8c33049afb3b60f7f2deda40976e3bb54da33769
soonkit-king <kitking2002@gmail.com>
2024-12-05 05:13:43 +0800

    Some main changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,50 @@
+import 'package:ebikesms/modules/global_import.dart';
+import 'package:ebikesms/modules/menu/screen/menu.dart';
+import 'package:ebikesms/modules/menu/sub-menu/ride_history/screen/ride_history.dart';
+import 'package:ebikesms/modules/menu/sub-menu/settings/screen/settings.dart';
+import 'package:ebikesms/modules/menu/sub-menu/time_top_up/screen/time_top_up.dart';
+import 'package:ebikesms/shared/widget/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'modules/auth/screen/login.dart'; // Adjust the path as needed
-// Adjust the path as needed
-// Adjust the path as needed
+import 'modules/auth/controller/user_storage_service.dart'; // Adjust the path as needed
 import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // Add dependency in pubspec.yaml
-// Adjust the path as needed
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() { //async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  //
+  // // Initialize secure storage
+  // const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+  //
+  // // Check if user ID exists in storage
+  // String? userId = await secureStorage.read(key: 'userId');
+  //
+  // runApp(MyApp(isLoggedIn: userId != null));
 
-  // Initialize secure storage
-  const FlutterSecureStorage secureStorage = FlutterSecureStorage();
-  
-  // Check if user ID exists in storage
-  String? userId = await secureStorage.read(key: 'userId');
-
-  runApp(MyApp(isLoggedIn: userId != null));
+  runApp(MyApp(isLoggedIn: true));
 }
 
 class MyApp extends StatelessWidget {
   final bool isLoggedIn;
-
-  const MyApp({super.key, required this.isLoggedIn});
+  const MyApp({
+    super.key,
+    required this.isLoggedIn
+  });
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        colorScheme: ColorScheme.fromSeed(seedColor: ColorConstant.darkBlue),
         useMaterial3: true,
-      ),
-      home: const LoginScreen()
-    );
-  }
-}
-
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _navigateToLogin() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const LoginScreen()),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _navigateToLogin,
-              child: const Text('Go to Login'),
-            ),
-          ],
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(fontFamily: 'Poppins'),
+          bodyMedium: TextStyle(fontFamily: 'Poppins'),
+          displayLarge: TextStyle(fontFamily: 'Poppins'),
+          displayMedium: TextStyle(fontFamily: 'Poppins'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const BottomNavBar(userId: 888, userType: "Rider"),
     );
   }
 }
